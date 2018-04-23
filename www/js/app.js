9462aac9216d459d1b3bc556327a7964c12d2060
Angel Careaga <acareagag@gmail.com>
2018-04-23 12:11:11 -0500

    Limpieza / Optimización / Versión 1 :zap: :memo: :bookmark:
    - Se limpio el código.
    - Se documento el código.
    - Se estandarizo el código.
    - Se lanza la versión de OneColors 1 pubica.

--- www/js/app.js
+++ www/js/app.js
@@ -1,157 +1,79 @@
-// Ionic Starter App
+/**
+ * Archivo de configuraciones generales y carga de modulos.
+ *
+ * Aquí puede especificar las URL de las páginas a utilizar, por ejemplo; 
+ * - HOME
+ * - ABOUT
+ *
+ * Javascript
+ *
+ * LICENSE: Este archivo fuente está sujeto a la licencia MIT
+ * que está disponible a través del siguiente URI:
+ * https://github.com/AngelCareaga/OneColors/blob/master/LICENSE
+ *
+ * @category   Javascript
+ * @package    js/
+ * @file       app.js
+ * @author     Ángel Careaga <dev.angelcareaga@gmail.com>
+ * @copyright  2018
+ * @license    MIT
+ * @version    1.0
+ */
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ngCordova'])
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            //cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-            // Don't remove this line unless you know what you are doing. It stops the viewport
-            // from snapping when text inputs are focused. Ionic handles this internally for
-            // a much nicer keyboard experience.
-            cordova.plugins.Keyboard.disableScroll(true);
+  /**
+   * RUN: REALIZA LAS PRIMERAS EJECUCIONES SOBRE LOS PLUGINS DE CORDOVA.
+   */
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      try {
+        if (window.statusbar) {
+          window.statusbar.show();
+          window.statusbar.backgroundColorByHexString("#2980b9");
+          window.statusbar.styleDefault();
         }
-        if (window.StatusBar) {
-            StatusBar.show();
-            StatusBar.backgroundColorByHexString("#2980b9");
-            StatusBar.styleDefault();
+        if (window.statusbar.isVisible) {
+          //console.log("La barra funciona");
         }
-        if (StatusBar.isVisible) {
-            //console.log("La barra funciona");
-        }
-
+      } catch (error) {
+        console.warn('Error: ' + error);
+      }
     });
-})
+  })
 
-
-
-.controller('mainController', function($scope, $cordovaCamera, $cordovaImagePicker) {
-
-    var colorThief = new ColorThief();
-
-    $scope.palette = [];
-    $scope.paletteTitulo = [];
-
-    document.addEventListener("deviceready", function() {
-
-        var options = {
-            quality: 100,
-            destinationType: Camera.DestinationType.DATA_URL,
-            sourceType: Camera.PictureSourceType.CAMERA,
-            allowEdit: true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 600,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false,
-            correctOrientation: false
-        };
-
-        var optionsPicker = {
-            maximumImagesCount: 10,
-            width: 800,
-            height: 800,
-            quality: 100
-        };
-
-        $scope.getCamera = function() {
-            $cordovaCamera.getPicture(options).then(function(imageData) {
-                var image = document.getElementById('imgDemo');
-                image.src = "data:image/jpeg;base64," + imageData;
-            }, function(err) {
-                // error
-            });
-        }
-
-        $scope.getPicker = function() {
-            $cordovaImagePicker.getPictures(optionsPicker)
-                .then(function(results) {
-                    var image = document.getElementById('imgDemo');
-                    if (results.length) {
-                        image.src = results[0];
-                    } else {
-                        alert("Selecciona una imagen.");
-                    }
-
-                }, function(error) {
-                    // error getting photos
-                });
-        }
-
-    }, false);
-
-    $scope.getColors = function() {
-        var a = document.getElementById("imgDemo");
-        try {
-            if (a.src) {
-
-                function rgbToHex(R,G,B) {return toHex(R)+toHex(G)+toHex(B)}
-                function toHex(n) {
-                 n = parseInt(n,10);
-                 if (isNaN(n)) return "00";
-                 n = Math.max(0,Math.min(n,255));
-                 return "0123456789ABCDEF".charAt((n-n%16)/16)
-                      + "0123456789ABCDEF".charAt(n%16);
-                }
-
-                //console.log("Obteniendo...");
-                var c = colorThief.getColor(a);
-                var p = colorThief.getPalette(a);
-                var pTitulo = colorThief.getPalette(a, 2);
-                $scope.palette = p;
-                $scope.paletteTitulo = pTitulo;
-                //var nuevoColor = pTitulo[0];
-                var nuevoColor = rgbToHex(pTitulo[0][0],pTitulo[0][1],pTitulo[0][2]);
-                //console.log("Nuevo color: " + pTitulo + ' Otro: ' + nuevoColor);
-                $scope.estiloTitulo={
-                    color:"white",
-                    backgroundColor:'#'+nuevoColor+''
-                  }
-
-            } else {
-                //console.log("Toma una imagen primero.");
-                alert("Toma una imagen primero.");
-            }
-        }catch(err)
-        {
-          //console.log("Error en: " + err);
-        }
-    }
-
-})
-
-.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  /**
+   * CONFIG: CREA LA CONFIGURACIÓN DE LAS RUTAS, Y EL USO DEL CONTROLLER.
+   */
+  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
     $stateProvider
-        .state('app', {
+      .state('app', {
         url: '/app',
         abstract: true,
-        templateUrl: 'templates/menu.html',
+        templateUrl: 'pages/menu.html',
         controller: 'mainController'
-    })
+      })
 
-    .state('app.search', {
-        url: '/search',
+      .state('app.home', {
+        url: '/home',
         views: {
-            'menuContent': {
-                templateUrl: 'templates/search.html'
-            }
+          'menuContent': {
+            templateUrl: 'pages/home.html'
+          }
         }
-    })
+      })
 
-    .state('app.browse', {
-        url: '/browse',
+      .state('app.about', {
+        url: '/about',
         views: {
-            'menuContent': {
-                templateUrl: 'templates/browse.html'
-            }
+          'menuContent': {
+            templateUrl: 'pages/about.html'
+          }
         }
-    });
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/search');
-    $ionicConfigProvider.tabs.position('bottom'); // other values: top
-})
+      });
+    $urlRouterProvider.otherwise('/app/home'); // Configura por defecto home como la pantalla principal.
+    $ionicConfigProvider.tabs.position('bottom'); // Obliga a posicionar las 'Tabs', en la parte inferior de la pantalla.
+  })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,67 +1,108 @@
+/**
+ * Archivo de funciones a utilizar en la obtención de los colores y fotografía.
+ *
+ * Aquí se desarrollan y declaran las funciones generales a utilizar en la página para
+ * la obtención de los colores, y cambio de estilos.
+ *
+ * Javascript
+ *
+ * LICENSE: Este archivo fuente está sujeto a la licencia MIT
+ * que está disponible a través del siguiente URI:
+ * https://github.com/AngelCareaga/OneColors/blob/master/LICENSE
+ *
+ * @category   Javascript
+ * @package    js/
+ * @file       controllers.js
+ * @author     Ángel Careaga <dev.angelcareaga@gmail.com>
+ * @copyright  2018
+ * @license    MIT
+ * @version    1.0
+ */
+
 angular.module('starter.controllers', [])
 
-.controller('mainController', function ($scope, $cordovaCamera, $cordovaImagePicker){
-  
-  var colorThief = new ColorThief();
+  /**
+   * CONTROLLER: CONTIENE TODAS LAS FUNCIONES A SER UTILIZADAS POR LA APLICACIÓN.
+   */
+  .controller('mainController', function ($scope, $cordovaCamera, $cordovaImagePicker) {
 
-  $scope.palette = [];
+    var colorThief = new ColorThief(); // Objeto para poder obtener los colores dominantes.
+    $scope.palette = [];
+    $scope.paletteTitulo = [];
 
-  document.addEventListener("deviceready", function () {
-
-    var options = {
-      quality: 100,
-      destinationType: Camera.DestinationType.DATA_URL,
-      sourceType: Camera.PictureSourceType.CAMERA,
-      allowEdit: true,
-      encodingType: Camera.EncodingType.JPEG,
-      targetWidth: 600,
-      popoverOptions: CameraPopoverOptions,
-      saveToPhotoAlbum: false,
-      correctOrientation:true
-    };
-
-    var optionsPicker = {
-     maximumImagesCount: 10,
-     width: 800,
-     height: 800,
-     quality: 80
-    };
-
-    $scope.getCamera = function (){
-      $cordovaCamera.getPicture(options).then(function(imageData) {
+    /**
+     * GETCAMERA: OBTIENE LA FOTOGRAFÍA Y LA CARGA AL CONTENEDOR.
+     */
+    $scope.getCamera = function () {
+      // Valores por defecto a utilizar en la opción de cámara.
+      var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG,
+        targetWidth: 600,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false,
+        correctOrientation: false
+      };
+      $cordovaCamera.getPicture(options).then(function (imageData) {
         var image = document.getElementById('imgDemo');
         image.src = "data:image/jpeg;base64," + imageData;
-      }, function(err) {
-        // error
+      }, function (err) {
+        console.log('CAMERA ERROR: ' + err);
       });
     }
 
-    $scope.getPicker = function (){
-      $cordovaImagePicker.getPictures(optionsPicker)
-      .then(function (results) {
-        var image = document.getElementById('imgDemo');
-        if(results.length){
-          image.src = results[0];
+    /**
+     * GETCOLORS: OBTIENE LOS COLORES DE LA IMAGEN CARGADA EN EL CONETEDOR.
+     */
+    $scope.getColors = function () {
+      var a = document.getElementById("imgDemo");
+      try {
+        if (a.src) {
+
+          /**
+           * Convierte color RGB a HEX.
+           * @param {*} R VALOR EN RED.
+           * @param {*} G VALOR EN GREEN.
+           * @param {*} B VALOR EN BLUE.
+           */
+          function rgbToHex(R, G, B) {
+            return toHex(R) + toHex(G) + toHex(B)
+          }
+
+          /**
+           * CONVIERTE VALOR EN RED, GREEN O BLUE A HEX.
+           * @param {*} n VALOR RED, GREEN, BLUE.
+           */
+          function toHex(n) {
+            n = parseInt(n, 10);
+            if (isNaN(n)) return "00";
+            n = Math.max(0, Math.min(n, 255));
+            return "0123456789ABCDEF".charAt((n - n % 16) / 16) +
+              "0123456789ABCDEF".charAt(n % 16);
+          }
+
+          // Realiza la obtención de los colores a partir del obteto y la imagen.
+          var p = colorThief.getPalette(a);
+          var pTitulo = colorThief.getPalette(a, 2);
+          $scope.palette = p;
+          $scope.paletteTitulo = pTitulo;
+          var nuevoColor = rgbToHex(pTitulo[0][0], pTitulo[0][1], pTitulo[0][2]);
+
+          // Cambia el color del fondo del contenedor de la imagen.
+          $scope.estiloTitulo = {
+            color: "white",
+            backgroundColor: '#' + nuevoColor + ''
+          }
+
         } else {
-          alert("Select an image!");
+          alert("Toma una imagen primero.");
         }
-
-      }, function(error) {
-        // error getting photos
-      });
+      } catch (err) {
+        console.log("GETCOLORS ERROR: " + err);
+      }
     }
 
-  }, false);
-
-  $scope.getColors = function (){
-    var a = document.getElementById("imgDemo");
-    if(a.src){
-      var c = colorThief.getColor(a);
-      var p = colorThief.getPalette(a, 5);
-      $scope.palette = p;
-    } else {
-      alert("Take a picture first!");
-    }
-  }
-
-});
+  });
